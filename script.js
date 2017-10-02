4d687cc4dcf716481182fa169fb7b19dd7005dc3
miquellifontana <miquellirfontana@gmail.com>
2017-10-02 20:16:09 -0300

    Implementado preenchimento básico do tabuleiro
    O tabuleiro está sendo preenchido de forma bem simples para possibilitar
    a continuação do desenvolvimento

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var field[][];
+var totalBombs = 3;
+var fieldColumns = 10;
+var fieldLines = 10;
+var field;
 
 function Slot() {
   this.isBomb = false;
-  this.wasSued = false;
+  this.processed = false;
   this.bombsAround = 0;
 }
 
 function reloadGame(){
+  field = new Array(fieldLines);
   setBombs();
   drawField();
 }
@@ -15,6 +19,21 @@ function reloadGame(){
  * Popula a matriz do tabuleiro
  */
 function setBombs(){
+  var bombsSetted = 0;
+
+  for (var i = 0; i < 10; i++) {
+    field[i] = new Array(fieldColumns);
+
+    if (bombsSetted < totalBombs){
+      var bomb = new Slot();
+      bomb.isBomb = true;
+      field[i][i] = bomb;
+    }
+
+  }
+  // while(bombsSetted < bombs){
+  //
+  // }
   // funcao distribuir as minas no tabuleiro{
   //   while bombas < bombas que eu preciso{
   //     tabuleiro[random1][random2] = bomba;
@@ -27,9 +46,23 @@ function setBombs(){
  * Desenha do tabuleiro a partir da matriz
  */
 function drawField(){
-// td.onclick = "metodo(this, i, j)"
+  var tableField = document.getElementById("bodyTableField");
+  tableField.innerHTML = "";
+
+  for (var i = 0; i < fieldLines; i++) {
+    var row = tableField.insertRow(i);
+
+    for (var j = 0; j < fieldColumns; j++) {
+      var slot = field[i][j];
+      var cell = row.insertCell(j);
+      cell.setAttribute("onclick", "onSlotClicked(" + i + ", " + j + ")");
+      if (slot != null ){
+        cell.innerHTML =slot.isBomb;
+      }
+    }
+  }
 }
 
-function onSlotClicked(slot, i, j){
+function onSlotClicked(i, j){
   drawField();
 }
